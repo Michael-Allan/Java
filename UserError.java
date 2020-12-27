43f3c9afb04c67c0daf1b9a32eadb5fe78088297
Michael Allan <mike@reluk.ca>
2020-12-26 22:42:10 -0500

    Formatting.

--- UserError.java
+++ UserError.java
@@ -3,10 +3,10 @@ package Java;
 
 /** Thrown on encountering an anomaly the user is likely in a position to correct.
   */
-public final class UserError extends Exception {
+public class UserError extends Exception {
+
 
     public UserError( String message ) { super( message ); }}
-
 
 
                                                         // Copyright © 2020  Michael Allan.  Licence MIT.
