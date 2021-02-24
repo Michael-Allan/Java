1ea405fad867f8aea82be3434794e300c1e74f58
Michael Allan <mike@reluk.ca>
2021-02-24 14:44:23 -0500

    Optimization.

--- CharBuffers.java
+++ CharBuffers.java
@@ -1,5 +1,7 @@
 package Java;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.nio.CharBuffer;
 
 
@@ -20,12 +22,35 @@ public final class CharBuffers {
         final DelimitableCharSequence s;
         if( buffer.hasArray() ) {
             final char[] array = buffer.array();
-            final int arrayStart = buffer.arrayOffset(); /* Non-zero in case e.g. of the buffer
-              being a slice of another buffer.  https://stackoverflow.com/a/24601336/2402790 */
-            if( arrayStart == 0 ) s = new ArrayCS( buffer, array );
-            else s = new OffsetArrayCS( buffer, array, arrayStart ); }
+            final int offset = buffer.arrayOffset(); // [BAO]
+            if( offset == 0 ) s = new ArrayCS( buffer, array );
+            else s = new OffsetArrayCS( buffer, array, offset ); }
         else s = new BufferCharSequence( buffer );
         return s; }
+
+
+
+    /** Commands the reader to transfer characters to the buffer by the most direct means,
+      * {@linkplain Reader#read(ch[],int,int) straight to the backing array} if the buffer has one.
+      * Advances the buffer position by the transfer count.
+      *
+      * <p>This method works around suboptimal implementations (e.g. in some versions of the JDK)
+      * of `reader.{@linkplain Reader#read(CharBuffer) read(buffer)}`.</p>
+      *
+      *     @return The number of characters transferred to the buffer.
+      */
+    public static int transferDirectly( final Reader reader, final CharBuffer buffer )
+          throws IOException {
+        final int n;
+        if( buffer.hasArray() ) {
+            final int offset = buffer.arrayOffset(); // [BAO]
+            final int p = buffer.position();
+            n = reader.read( buffer.array(), offset + p, buffer.remaining() ); /* A transfer in one step,
+              what the JDK itself ought to be doing.  Instead its reader (of JDK 15, at last inspection)
+              transfers 1) from the source to a temporary array, then 2) from the array to the buffer. */
+            if( n > 0 ) buffer.position( p + n ); }
+        else n = reader.read( buffer );
+        return n; }
 
 
 
@@ -108,6 +133,13 @@ public final class CharBuffers {
 
 
         public @Override int start() { return super.start() - arrayStart; }}} // Relativizing.
+
+
+
+// NOTE
+// ────
+//   BAO  Backing-array offset.  This is non-zero in case e.g. of an array-backed buffer formed as
+//        a slice of another buffer.  https://stackoverflow.com/a/24601336/2402790
 
 
 
