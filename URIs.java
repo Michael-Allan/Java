7441844f9594dd520a5e728f663696e9355abb79
Michael Allan <mike@reluk.ca>
2022-05-26 02:19:46 -0400

    Reference repair.

--- URIs.java
+++ URIs.java
@@ -18,8 +18,8 @@ public final class URIs {
       * of its leading scheme component and ‘:’ separator.
       *
       *     @see java.util.regex.Matcher#lookingAt()
-      *     @see <a href='https://tools.ietf.org/html/rfc3986#section-3'>URI</a>
-      *     @see <a href='https://tools.ietf.org/html/rfc3986#section-3.1'>Scheme</a>
+      *     @see <a href='https://datatracker.ietf.org/doc/html/rfc3986#section-3'>URI</a>
+      *     @see <a href='https://datatracker.ietf.org/doc/html/rfc3986#section-3.1'>Scheme</a>
       */
     public static final Pattern schemedPattern = Pattern.compile( "[a-z0-9A-Z][a-z0-9A-Z+.-]*:" );
 
