460230c8cc9396b0b7b6b34eca2b335d2b4299de
Michael Allan <mike@reluk.ca>
2022-11-15 08:20:22 -0500

    Utility: marking of isolated text lines or strings.

--- CharacterPointer.java
+++ CharacterPointer.java
@@ -30,7 +30,7 @@ public final class CharacterPointer {
 
 
 
-    /** A copy of the text line in which the character occurs, up to and including any terminal newline.
+    /** A copy of the text line in which the character occurs, with or without any terminal newline.
       */
     public final String line;
 
@@ -43,9 +43,22 @@ public final class CharacterPointer {
 
 
 
-    /** Returns an echo of the text line, plus a column mark.
+    /** Returns a multi-line string comprising an echo of the text line together with a column marker.
       */
-    public String markedLine() {
+    public String markedLine() { return markedLine( line, column ); }
+
+
+
+    /** Returns a multi-line string comprising an echo of a text line together with a column marker.
+      *
+      *     @param line The text line to be marked, with or without any terminal newline.
+      *     @param column The columnar offset at which to mark the line.  Columnar offsets are zero based
+      *       and measured in terms of grapheme clusters, beginning with the first cluster at offset zero
+      *       in the text line.
+      *     @see <a href='https://unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries'>
+      *       Grapheme cluster boundaries in Unicode text segmentation</a>
+      */
+    public static String markedLine( final String line, final int column ) {
         final StringBuilder b = new StringBuilder();
         for( int ch, c = 0, cN = line.length(); c < cN; c += charCount(ch) ) { // Sanitize:
             ch = line.codePointAt( c ); // By full code point, not to miss the problematic ones.
@@ -53,7 +66,17 @@ public final class CharacterPointer {
         b.append( '\n' );
         for( int c = column; c > 0; --c ) b.append( ' ' );
         b.append( '^' );
-        return b.toString(); }}
+        return b.toString(); }
+
+
+
+    /** Returns a multi-line string comprising an echo of a text line together with a column marker.
+      *
+      *     @param line The text line to be marked, with or without any terminal newline.
+      *     @param c The zero-based offset of the character whose column to mark.
+      */
+    public static String markedLine( final String line, final int c, final GraphemeClusterCounter gcc ) {
+        return markedLine( line, /*column*/gcc.clusterCount(line,0,c) ); }}
 
 
 
