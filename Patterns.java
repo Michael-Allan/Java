08f5cbb252a51c5fc235238764ce971d4a61d21a
Michael Allan <mike@reluk.ca>
2022-12-26 09:22:23 -0500

    Metacharacters and quoting.

--- Patterns.java
+++ Patterns.java
@@ -30,7 +30,30 @@ public final class Patterns {
         if( (f & DOTALL                  ) != 0 ) b.append( 's' );
         if( (f & UNICODE_CHARACTER_CLASS ) != 0 ) b.append( 'U' );
         if( (f & UNICODE_CASE            ) != 0 ) b.append( 'u' );
-        if( (f & COMMENTS                ) != 0 ) b.append( 'x' ); }}
+        if( (f & COMMENTS                ) != 0 ) b.append( 'x' ); }
+
+
+
+    /** A string comprising all characters that have special meaning outside of a character class.
+      */
+    public static final String metacharacters = "\\^.$|()*+?[]{}";
+
+
+
+    /** Appends to `b` the characters of `seq`, escaping by backslash `\` any that is a metacharacter.
+      * Unlike `Pattern.quote`, which would simply bracket the whole with `\Q` and `\E` (JDK 18),
+      * this method escapes only what needs escaping.
+      *
+      *     @see #metacharacters
+      *     @see Pattern#quote(String)
+      */
+    public static void quote( final CharSequence seq, final StringBuilder b ) {
+        final int cN = seq.length();
+        for( int c = 0; c < cN; ++c ) {
+            final char ch = seq.charAt( c );
+            if( !( ch == ' '  ||  '_' <= ch && ch <= 'z'  ||  metacharacters.indexOf(ch) < 0 )) {
+                b.append( '\\' ); }
+            b.append( ch ); }}}
 
 
 
